a6baa9b71d1143d4bfcc329b19fcfed5387a085e
Milan <meetmmpatel@gmail.com>
2021-06-04 12:19:43 -0400

    Added Swagger Docs on the api controller class

--- onetomany_bidirectional/src/main/java/com/oneToMany/api/controller/RoleController.java
+++ onetomany_bidirectional/src/main/java/com/oneToMany/api/controller/RoleController.java
@@ -2,8 +2,14 @@ package com.oneToMany.api.controller;
 
 import com.oneToMany.api.exception.DataNotFoundException;
 import com.oneToMany.api.model.Role;
-import com.oneToMany.api.repository.RoleRepository;
 import com.oneToMany.api.service.RoleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/role")
+@Tag(name = "Role API", description = "Role API controller")
 public class RoleController {
   
   @Autowired
@@ -22,26 +29,44 @@ public class RoleController {
 	return roleService.addRoleWithUser(role);
   }
   
+  @Operation(summary = "Get the list of all the roles", tags = {"roles"})
+  @ApiResponses(value = {@ApiResponse(responseCode = "200",
+		  description = "Found the list of roles",
+		  content = {@Content(mediaType = "application/json",
+				  array = @ArraySchema(schema = @Schema(implementation = Role.class))
+		  ),})})
+  @ApiResponse(responseCode = "400", description = "Invalid Request"
+		  , content = @Content)
   @GetMapping("/list")
   public ResponseEntity<?> getListOfRoles() {
 	return roleService.readListOfRole();
   }
   
+  @Operation(summary = "Get Role by ID")
+  @ApiResponses(value = {@ApiResponse(responseCode = "200",
+		  description = "Found Role by specific ID",
+		  content = {@Content(mediaType = "application/json",
+				  schema = @Schema(implementation = Role.class)
+		  ),})})
+  @ApiResponse(responseCode = "404", description = "ID not found in database"
+		  , content = @Content)
   @GetMapping("/{id}")
   public ResponseEntity<?> getRoleByID(@PathVariable(value = "id") long id)
 		  throws DataNotFoundException {
 	return roleService.readRole(id);
   }
   
+  
   @DeleteMapping("/delete/{id}")
   public ResponseEntity<?> deleteRole(@PathVariable(value = "id") long id) {
 	return roleService.deleteRoleByID(id);
   }
   
+  
   @PutMapping("update/{id}")
-  public ResponseEntity<?> updateRole(@PathVariable(value = "id")long id,
+  public ResponseEntity<?> updateRole(@PathVariable(value = "id") long id,
 									  @RequestBody Role role) throws DataNotFoundException {
-    return roleService.updateRoleByID(id,role);
+	return roleService.updateRoleByID(id, role);
   }
   
 }
